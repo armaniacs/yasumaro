c139d1e7e7a87de2d075b76445c31e7e7d302e19
Yasuhiro ARAKI <iron@araki.net>
2026-07-11 09:58:26 +0900

    chore(lint): add ESLint with @typescript-eslint rules (M25)

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import sveltePlugin from 'eslint-plugin-svelte';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', 'testDir/', 'src/**/__tests__/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    ignores: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  ...sveltePlugin.configs['flat/recommended'],
+];
